23c346f7b464162cd69d86dac33a668755007d61
krajagop240380 <karthick.dreams@gmail.com>
2022-01-08 15:05:09 +0530

    V3 for Deploy Lambda Function Using Terraform

--- my_lambda_function.tf
+++ my_lambda_function.tf
@@ -1,21 +1,21 @@
-locals {
-	lambda-zip-location = "outputs/my_function.zip"
-}
+	locals {
+		lambda-zip-location = "outputs/my_function.zip"
+	}
 
-data "archive_file" "init" {
-  type        = "zip"
-  source_file = "my_function.py
-  output_path = locals.lambda-zip-location
-}
+	data "archive_file" "init" {
+	  type        = "zip"
+	  source_file = "my_function.py"
+	  output_path = local.lambda-zip-location
+	}
 
-resource "aws_lambda_function" "test_lambda" {
-  filename      = locals.lambda-zip-location
-  function_name = "my_function"
-  role          = aws_iam_role.my_lambda_iam_role.arn
-  handler       = "my_function.hello"
-	
-  source_code_hash = filebase64sha256("locals.lambda-zip-location")	
-	
-  runtime = "python3.7"
-  
-}
+	resource "aws_lambda_function" "test_lambda" {
+	  filename      = local.lambda-zip-location
+	  function_name = "my_function"
+	  role          = aws_iam_role.my_lambda_iam_role.arn
+	  handler       = "my_function.my_lamda_function"
+		
+	  #source_code_hash = filebase64sha256("locals.lambda-zip-location")	
+		
+	  runtime = "python3.7"
+	  
+	}
